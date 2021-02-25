5c9c9df005a3d12f1b910debb1b5285349141c42
thanhndv212 <dinhvinhthanhnguyen@gmail.com>
2021-02-25 14:18:38 +0100

    done testing 2 DOF

--- examples/regressor_dp.py
+++ examples/regressor_dp.py
@@ -1,19 +1,16 @@
-#
-# In this short script, we show how to use RobotWrapper
-# integrating different kinds of viewers
-#
 import pinocchio as pin
 from pinocchio.robot_wrapper import RobotWrapper
 from pinocchio.visualize import (GepettoVisualizer, MeshcatVisualizer)
 from sys import argv
 
+from tabulate import tabulate
 import time 
 from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 from numpy.linalg import norm, solve
 from pinocchio.utils import *
-
+from scipy import linalg
 import os
 from os.path import dirname, join, abspath
 # If you want to visualize the robot in this example,
@@ -40,63 +37,41 @@ mesh_dir = model_path
 urdf_filename = "2DOF_description.urdf"
 urdf_model_path = join(join(model_path,"2DOF_description/urdf"),urdf_filename)
 
-#urdf_filename = "double_pendulum.urdf"
-#urdf_model_path = join(join(model_path,"double_pendulum_description/urdf"),urdf_filename)
-robot = RobotWrapper.BuildFromURDF(urdf_model_path, mesh_dir, pin.JointModelFreeFlyer())
+# urdf_filename = "double_pendulum.urdf"
+# urdf_model_path = join(join(model_path,"double_pendulum_description/urdf"),urdf_filename)
+robot = RobotWrapper.BuildFromURDF(urdf_model_path, mesh_dir)
 
 model = robot.model
 data = robot.data	
 nq, nv = model.nq, model.nv
 
 #numbers of samples
-N = 999 
+N = 49
 
 #test case
 q = pin.neutral(model)
-#q = np.array([0, 0, 0, 0, 0, 0, 0, 1, 1])
-
-
-
-v = pin.utils.rand(model.nv)
-#v = np.array([1, 1, 1, 1, 1, 1, 1, 1])
+# q = np.array([ 0.5, 0.5])
+v = pin.utils.zero(model.nv)
 
 a = pin.utils.zero(model.nv)
-#a = np.array([1, 1, 1, 1, 1, 1, 1, 1])
-
-print('q = ', q)
-print('v = ', v)
-print('a = ', a)
 
 
+print(model)
 TAU = pin.rnea(model, data, q, v, a)
-#print('tau = ', tau)
-
-
 W = pin.computeJointTorqueRegressor(model, data, q, v, a)
-#print('Y = ', Y)
 
-#J = pin.crba(model, data, q)
-#print('J = ', J)
-
-
-
-
-
-
-
-
-
-
+##################
+print('*******************************************')
+##################
 
 #generate waypoints
-Q = np.array([[0, 0 , 0 , 0 , 0 , 0 , 0 , 0, 0]])
-DQ = np.array([[ 0 , 0 , 0 , 0 , 0 , 0 , 0, 0]])
-DDQ = np.array([[ 0 , 0 , 0 , 0 , 0 , 0 , 0, 0]])
 
 t = 0
 eps = 1e-4
-a = 0
-b = 3
+A1 = np.random.uniform(low = 5, high = 10, size=(3,))
+A2 = np.random.uniform(low = -7, high = 10, size=(3,))
+a = -0.7
+b = 10
 c = 2
 d = 2
 q_l2 = q_l1 = 0.0
@@ -104,65 +79,130 @@ q_temp = q
 v_temp = v
 a_temp = a
 for i in range(N):	
-	q_l1 =  a + b*t + c*t*t + d*t*t*t
-	q_l2 =  a + b*t + c*t*t + d*t*t*t
-	q_temp =  np.array([0,0,0, 0,0,0, 0,q_l1,q_l2])
-	qd_l1 = b + 2*c*t + 3*d*t*t
-	qd_l2 = b + 2*c*t + 3*d*t*t
-	v_temp = np.array([0,0,0, 0,0,0, 0,qd_l1,qd_l2])
-	qdd_l1 = 2*c + 6*d*t
-	qdd_l2 = 2*c + 6*d*t
-	a_temp = np.array([0,0,0, 0,0,0, 0,qdd_l1,qdd_l2])
+	q_l1 =  a + A1[0]*t + A1[1]*t*t + A1[2]*t*t*t
+	q_l2 =  a + A2[0]*t + A2[1]*t*t + A2[2]*t*t*t
+	# q_temp =  np.array([q_l1,q_l2])
+	q_temp = np.random.uniform(low = -10, high = 10, size=(2,))
+	q = np.vstack((q,q_temp))
+	qd_l1 = A1[0] + 2*A1[1]*t + 3*A1[2]*t*t
+	qd_l2 = A2[0] + 2*A2[1]*t + 3*A2[2]*t*t
+	# v_temp = np.array([qd_l1,qd_l2])
+	v_temp = np.random.uniform(low = -10, high = 10, size=(2,))
+	qdd_l1 = 2*A1[1] + 6*A1[2]*t
+	qdd_l2 = 2*A2[1] + 6*A2[2]*t
+	# a_temp = np.array([qdd_l1,qdd_l2])
+	a_temp = np.random.uniform(low = -10, high = 10, size=(2,))
 	t = t+ eps
 	tau_temp = pin.rnea(model, data, q_temp, v_temp, a_temp)
 	W_temp = pin.computeJointTorqueRegressor(model, data, q_temp, v_temp, a_temp)
-	TAU = np.vstack((TAU,tau_temp))
+	TAU = np.append(TAU,tau_temp)
+	W = np.vstack((W,W_temp))
+	# print(robot.gravity(q_temp))
 
-	W = np.vstack((W,W_temp)) #size of 8000 x 30
+#inertial parameters of link2 from urdf model
+phi1 = model.inertias[1].toDynamicParameters()
+print(model.inertias)
+phi2 = model.inertias[2].toDynamicParameters()
+phi = np.round(np.append(phi1, phi2),6)
+print('standard paramaters: ')
+params_full = ['m1', 'mx1','my1','mz1','Ixx1',
+'Ixy1','Iyy1','Ixz1', 'Iyz1','Izz1',
+'m2', 'mx2','my2','mz2','Ixx2',
+'Ixy2','Iyy2','Ixz2', 'Iyz2','Izz2']
+std_table = [params_full, phi]
+print(tabulate(std_table))
+# print('condition number of regressor W: ', np.linalg.cond(W))
+##################
+print('*******************************************')
+##################
 
+params_e = []
+params_r = [] 
 
-TAU = TAU.T #transpose the joint torque matrix, size of 8x1000, the last 2 rows corresponds to 2 joint torque
+#eliminate columns crspnd. non-contributing parameters
+tol_e = 1e-16
+diag_W2 = np.diag(np.dot(W.T,W)) 
+phi_e = np.array([])
+for i in range(diag_W2.shape[0]):
+	if diag_W2[i] < tol_e:
+		phi_e = np.append(phi_e, i)
+		params_e.append(params_full[i])
+	else:
+		params_r.append(params_full[i])
+# phi_e = np.where(diag_W2 < tol_e, diag_W2, np.delete(diag_W2,,1))
+phi_e = phi_e.astype(int)
+W_e = np.delete(W, phi_e, 1)
+npb = len(params_r)
+# print('eliminated parameters: ',params_e)
 
-#inertial parameters of link2 from urdf 
-P = model.inertias[1].toDynamicParameters()
-for i in range(2):
-	p_i = model.inertias[i+2].toDynamicParameters()
-	P = np.append(P, p_i)
+#QR decompostion, rank revealing
+params_rsorted = []
 
+epsilon = np.finfo(float).eps# machine epsilon
+Q, R , P= linalg.qr(W_e, pivoting=True)
+# print(np.diag(R))
+# print(Q.shape[0])
+for ind in P: 
+	params_rsorted.append(params_r[ind])
+# print('reduced parameters sorted:',params_rsorted)
+# print(tabulate([W_e[7, :],W_e[8,:],W_e[9,:],W_e[10,:]]))
+tolpal = W_e.shape[0]*abs(np.diag(R).max())*epsilon#rank revealing tolerance
+for i in range(np.diag(R).shape[0]):
+	if abs(np.diag(R)[i]) < tolpal:
+		numrank_W = i
+R1 = R[0:numrank_W,0:numrank_W]
+Q1 = Q[:,0:numrank_W]
+R2 = R[0:numrank_W,numrank_W:npb]
+beta = np.round(np.dot(np.linalg.inv(R1),R2),6)
+# beta = np.where(beta > tol_e, beta, 0)#remove small beta close to zero 
+phi_b = np.round(np.dot(np.linalg.inv(R1),np.dot(Q1.T,TAU)),6)
+W_b = np.dot(Q1,R1)
+params_idp = params_rsorted[:numrank_W]
+params_rgp = params_rsorted[numrank_W]
+# print('idpnd. parameters: ', params_idp)
+# print('regrouped parameters: ', params_rgp)
+print('condition number of base regressor: ',np.linalg.cond(W_b))
+U, S, VT = np.linalg.svd(W_b)
+print('singular values of base regressor:', S)
+##################
+print('*******************************************')
+##################
 
-#calculate joint torque between link1 and link2
-TAU_est = np.dot(W,P)
+params_base = []
+for i in range(numrank_W):
+	if beta[i] == 0:	
+		params_base.append(params_idp[i])
+		
+	else:
+		params_base.append(params_idp[i] + ' + '+str(round(float(beta[i]),6)) + '*'+ str(params_rgp))
 
-tau2_est = np.array([TAU_est[7]])
-for i in range(999):
-	tau2_est = np.append(tau2_est, TAU_est[8*(i+1)-1 + 8])
+print('base parameters and their identified values: ')
+table = [params_base, phi_b]
+print(tabulate(table))
+# 
 
-# joint 2 by rnea()
-tau2 = TAU[7,:]
-print(tau2.shape[0])
-#plot measured tau and estimated tau 
-fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.scatter(tau2, tau2_est)
-m, b = np.polyfit(tau2, tau2_est,1)
-print(m, b)
-plt.plot(tau2, m*tau2 + b,'r', label='y={:.2f}x+{:.2f}'.format(m,b))
-plt.xlabel("Joint torque 2 by rnea() function")
-plt.ylabel("Joint torque 2 estimated by regressor matrix")
-plt.legend()
-plt.grid()
-plt.show()
+# #calculate joint torque by estimating from regressor and model params
+# TAU_est = np.dot(W,phi)
+# tau1_est = np.array([TAU_est[6]])
+# for i in range(999):
+# 	tau1_est = np.append(tau1_est, TAU_est[8*(i+1)-2 + 8])
+# tau2_est = np.array([TAU_est[7]])
+# for j in range(999):
+# 	tau2_est = np.append(tau2_est, TAU_est[8*(j+1)-1 + 8])
 
+#plot measured tau and estimated joint2 
+# fig = plt.figure()
+# ax = fig.add_subplot(111)
+# ax.scatter(TAU, TAU_est)
+# m, b = np.polyfit(tau2, tau2_est,1)
+# print(m, b)
+# plt.plot(tau2, m*tau2 + b,'r', label='y={:.2f}x+{:.2f}'.format(m,b))
+# plt.xlabel("Joint torque 2 by rnea() function")
+# plt.ylabel("Joint torque 2 estimated by regressor matrix")
+# plt.legend()
+# plt.grid()
+# plt.show()
 
-
-#compute Joint Torque regressor
-	#w = pin.computeJointTorqueRegressor(model, data, q, v, a)
-	#todo: append to matrix
-#todo: naming columns
-#joint torques by rnea()
-	#tau = pin.rnea(model, data, q, v, a)   
-#decomposition 
-#todo: print out table of combined values after decomposition 
 
 
 #display 
@@ -172,9 +212,9 @@ if VISUALIZER:
 	robot.setVisualizer(VISUALIZER())
 	robot.initViewer()
 	robot.loadViewerModel("pinocchio")
-	for k in range(q.shape[1]):
+	for k in range(q.shape[0]):
 		t0 = time.time()
-		robot.display(q[:, k])
+		robot.display(q[k,:])
 		t1 = time.time()
 		elapsed_time = t1 - t0
 		if elapsed_time < dt:
